13bb446a5f55f5e1f3ed39b9d32600812324d373
Manish561318 <20030726@students.koi.edu.au>
2026-04-13 09:56:45 +0545

    Added register page and homepage navigation

--- members/views.py
+++ members/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'members/home.html')
+
+
+def register(request):
+    return render(request, 'members/register.html')
